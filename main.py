8a3a78d1ac0497dc57137445b5fcbc7d050cd2f6
Ekaterina Baeva <ameli.pullen@mail.ru>
2021-09-15 21:53:18 +0300

    fixed mistake

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def scrape_article(key, url):
     articles = soup.find_all('article')
     new_list = []
     for article in articles:
-        headlines = article.h2.a.text
+        headlines = article.find('h2', class_='tm-article-snippet__title tm-article-snippet__title_h2').text
         date_post = article.find('span', class_='tm-article-snippet__datetime-published').text
         post_link = article.find('a', class_='tm-article-snippet__title-link').attrs.get('href')
         post_text = article.find('div', class_='tm-article-body tm-article-snippet__lead').text
         for words in key:
             if (words.lower() in headlines.lower()) or (words.lower() in post_text.lower()):
-                print(f'Дата: {date_post} - Заголовок: {headlines} - Ссылка: {post_link}')
+                print(f'Дата: {date_post} - Заголовок: {headlines} - Ссылка: https://habr.com{post_link}')
                 new_list.append([date_post, headlines, post_link])
     return new_list
 
